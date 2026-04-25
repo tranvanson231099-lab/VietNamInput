25ccab2d8f2289dab252ec2f194aa977918f4fe6
tranvanson231099 <tranvanson231099@gmail.com>
2026-04-25 09:53:53 +0000

    c

--- background.js
+++ background.js
@@ -2,7 +2,11 @@
 
 import { registerImeListeners } from './src/logic/keyboard_handler.js';
 
-// Đăng ký các hàm lắng nghe sự kiện của IME
-registerImeListeners();
+// Wrap the listener registration in the onInstalled event
+chrome.runtime.onInstalled.addListener(() => {
+  // Đăng ký các hàm lắng nghe sự kiện của IME
+  registerImeListeners();
+  console.log("Vietnam Input IME listeners registered.");
+});
 
-console.log("Vietnam Input background script loaded and listeners registered.");
+console.log("Vietnam Input background script loaded.");
